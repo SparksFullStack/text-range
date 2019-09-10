e8e29462bda734c42b4568dde3109ebd01c6f4af
SparksFullStack <csparks@antioch.edu>
2019-09-10 11:03:17 -0400

    solution partially implemented

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function mainFunc() {
     const range = document.createRange();
-    const findingText = document.getElementsByClassName('finding-text')[0].innerHTML.split(' ');
-    console.log('finding', findingText);
+    const textNode = document.getElementsByClassName('finding-text')[0]
+    const findingText = textNode.innerHTML.split(' ');
+    
+    let start = 0;
+    let end = 0;
+
+    // NOTE: I can set the range for the start (although it stays at 0), but can't set the end.
+    for (let i = 0; i < findingText.length; i++) {
+        let currentWord = findingText[i];
+        end = currentWord.length + start;
+        range.setStart(textNode, start);
+        //range.setEnd(textNode, end);
+        console.log('range', range);
+    }
 }
