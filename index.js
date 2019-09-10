9dd953388eb886133229519380d6df116c6ccc5c
SparksFullStack <csparks@antioch.edu>
2019-09-10 10:53:30 -0400

    added function that sets up the range, gets the text from the element, and splits it

---
+++ index.js
@@ -0,0 +1,5 @@
+function mainFunc() {
+    const range = document.createRange();
+    const findingText = document.getElementsByClassName('finding-text')[0].innerHTML.split(' ');
+    console.log('finding', findingText);
+}
