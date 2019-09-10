920775b53b063637070ce8e88b9a608353a983c2
SparksFullStack <csparks@antioch.edu>
2019-09-10 11:12:58 -0400

    solution partially implemented

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function mainFunc() {
     const range = document.createRange();
-    const findingText = document.getElementsByClassName('finding-text')[0].innerHTML.split(' ');
-    console.log('finding', findingText);
+    const textNode = $('.finding-text');
+    const findingText = textNode[0].innerText.split(' ');
+    
+    // const findingText = textNode.innerHTML.split(' ');
+    // document.getElementsByClassName('finding-text')[0]
+    
+    const testing = $(findingText[0]).wrapInner("<span class='testing'></span>")
+    console.log(testing);
+    // // NOTE: I can set the range for the start (although it stays at 0), but can't set the end.
+    // for (let i = 0; i < findingText.length; i++) {
+        
+    // }
 }
